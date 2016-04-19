9a2f153a1beaded539cd94ea8a3282d517075a81
Sanjay <Sanjay@Sanjay>
2016-04-19 14:34:00 -0700

    Sanjay

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Getting started with bootstrap</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+
+
+<script src="js/jquery.js"></script>
+<script src='js/bootstrap.min.js'></script>
+
+</body>
+</html>
